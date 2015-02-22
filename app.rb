700ccf9b26f1d69b99886135532609a178f60bc1
Alexandria Cattron <acattron@gmail.com>
2015-02-22 13:15:45 -0800

    Added new prompt and changed some styling for chat

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ module ChatDemo
 
       prompts = ["The fight has been raging for three days. The teachers on their end, toting rulers, eraser canons and textbook grenades, and the students on the other, with only school-approved items found in their bookbags. Both sides have suffered casualties and people are wondering, what happened three days ago at Winston Waters High that started...", 
         "You take a sip from your drink and feel different. That may be because your torso has an extra arm protruding from it. Another sip, another arm. Then a wing. What happens if you finish the drink?",
-        "During your trip to Turkey, you check into an old hotel. After settling yourself you notice a weathered chest in the corner. Upon opening it, war relics from the First World War and the Ottoman Empire lay before you. What stories do you find?",
+        "You and your sister win a talking Gold Fish at a fair.",
         "Being a famous rock star is hard. Especially when an alien invasion hits mid-concert. As lasers and abductions abound, do you think your sick beats can stop this catastrophe? Write about how you attempted to fight off the aliens and whether or not you succeeded.",
         "Your family isn't cooperating with your writing career, so you've decided to go on strike. Write a list of demands that must be met in order for you to return to your chores and household responsibilities. (Don't forget to make a concession or two to speed up the negotiation process.)"]
       
